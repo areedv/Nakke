dc68cdfe121ede018c72ee85975c5fcde6da67a0
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-05-19 12:59:17 +0200

    indent

--- R/nakkeStandard.R
+++ R/nakkeStandard.R
@@ -8,31 +8,31 @@
 #' @export
 
 nakkeStandard <- function(input, output, session) {
-     sessionName <- session$ns("name")
-     # namespace id comes with an extra '-name'. Remove it
-     sessionName <- gsub("-name", "", sessionName)
-     if (sessionName == "figAndeler") {
-          #get_uc_outside_namespace_from_additional_args
-          plotObj <- reactive({
-               FigAndeler(RegData=RegData,
-                          valgtVar = "Alder",
-                          erMann=as.numeric(input$erMann),
-                          minald = as.numeric(input$alder[1]),
-                          maxald = as.numeric(input$alder[2]),
-                          reshID = as.numeric(input$avdeling)
-               )
-          })
-     }
-     if (sessionName == "report2") {
-          #get_uc_outside_namespace_from_additional_args
-          plotObj <- reactive({
-               FigAndeler(RegData=RegData,
-                          valgtVar = "Alder",
-                          erMann=as.numeric(input$erMann),
-                          minald = as.numeric(input$alder[1]),
-                          maxald = as.numeric(input$alder[2]),
-                          reshID = 103469)
-          })
-     }
-     return(plotObj)
+  sessionName <- session$ns("name")
+  # namespace id comes with an extra '-name'. Remove it
+  sessionName <- gsub("-name", "", sessionName)
+  if (sessionName == "figAndeler") {
+    #get_uc_outside_namespace_from_additional_args
+    plotObj <- reactive({
+      FigAndeler(RegData=RegData,
+                 valgtVar = "Alder",
+                 erMann=as.numeric(input$erMann),
+                 minald = as.numeric(input$alder[1]),
+                 maxald = as.numeric(input$alder[2]),
+                 reshID = as.numeric(input$avdeling)
+      )
+    })
+  }
+  if (sessionName == "report2") {
+    #get_uc_outside_namespace_from_additional_args
+    plotObj <- reactive({
+      FigAndeler(RegData=RegData,
+                 valgtVar = "Alder",
+                 erMann=as.numeric(input$erMann),
+                 minald = as.numeric(input$alder[1]),
+                 maxald = as.numeric(input$alder[2]),
+                 reshID = 103469)
+    })
+  }
+  return(plotObj)
 }
