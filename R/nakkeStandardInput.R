c702e18b829d97c53405b36a39c88e70452f0f64
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-05-19 15:23:03 +0200

    change default dates

--- R/nakkeStandardInput.R
+++ R/nakkeStandardInput.R
@@ -23,8 +23,8 @@ nakkeStandardInput <- function(id, label = "Brukervalg") {
     sliderInput(ns("alder"), label = h3("Alder"), min = 0,
                 max = 130, value = c(0, 130)
     ),
-    dateRangeInput(ns("periode"), label = h3("Periode"),
-                   separator="til", language="nb"),
+    dateRangeInput(ns("periode"), start = "2012-01-01", end = Sys.Date(),
+                   label = h3("Periode"), separator="til", language="nb"),
     selectInput(ns("avdeling"), label = h3("Avdeling"), reshList)
   )
 }
