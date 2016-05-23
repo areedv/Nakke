416e1b47d7c1bf1af1bce57ecca7c36722e3350d
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-05-23 13:39:00 +0200

    proper vars

--- R/valgtVarFigGjsnGrVar.R
+++ R/valgtVarFigGjsnGrVar.R
@@ -10,36 +10,14 @@
 valgtVarFigGjsnGrVar <- function() {
 
   valgtVarListFigGjsnGrVar <-
-    list("Aldersfordeling"="Alder",
-         "Antallnivåoperert"="AntallNivaaOpr",
-         "Erdetgittantibiotikaprofylakse"="Antibiotika",
-         "Arbeidsstatus12mndetteroperasjon"="Arbeidstaus12mnd",
-         "Arbeidsstatus3mndetteroperasjon"="Arbeidstaus3mnd",
-         "Arbeidsstatusføroperasjon"="ArbeidstausPreOp",
-         "ASAgrad"="ASAgrad",
-         "PasientenesBMIBodyMassIndex"="BMI",
-         "HelsetilstandAngst"="EqAngstPreOp",
-         "Søkterstatning"="ErstatningPreOp",
-         "Fornøydhetmedbehandlingapåsykehuset12mnd"="FornoydBeh12mnd",
-         "Fornøydhetmedbehandlingapåsykehuset3mnd"="FornoydBeh3mnd",
-         "Hastegrad"="OperasjonsKategori",
-         "Antallliggedøgnpostoperativt"="LiggeDognPostop",
-         "Totaltantallliggedøgn"="LiggeDognTotalt",
-         "Morsmål"="Morsmal",
-         "Nytteavoperasjon12mnd"="NytteOpr12mnd",
-         "Nytteavoperasjon3mnd"="NytteOpr3mnd",
-         "Paresegradføroperasjon"="OprIndikPareseGrad",
-         "Røykerpasienten"="Roker",
-         "Harpasientenfåttsårdren"="Saardren",
-         "Sivilstatus"="SivilStatus",
-         "Hyppighetavsmertestillendeføroperasjonen"="SmertestillBrukPreOp",
-         "Snuserpasienten"="Snuser",
-         "Varighetavutstrålendearmsmerter"="SymptVarighetArmer",
-         "Varighetavnakkehodesmerter"="SymptVarighetNakkeHode",
-         "Erpasiententidligereoperert"="TidlOpr",
-         "Antalltidligereoperasjoner"="TidlOprAntall",
-         "Søktuføretrygd"="UforetrygdPreOp",
-         "Utdanningsnivå"="Utdanning")
+    list("Alder (år)"="Alder",
+         "EMS hos Myelopatipasienter før"="EMSscorePreOp",
+         "Total knivtid"="KnivtidTotalMin",
+         "NDI før operasjon"="NDIscorePreOp",
+         "Liggetid etter operasjon"="LiggeDognPostop",
+         "Antall liggedøgn, totalt"="LiggeDognTotalt",
+         "NSR, arm før operasjon"="NRSsmerteArmPreOp",
+         "NSR, nakke før operasjon"="NRSsmerteNakkePreOp")
 
   return(valgtVarListFigGjsnGrVar)
 }
