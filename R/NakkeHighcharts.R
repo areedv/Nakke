1a1f295b0b97c51573e8bda5be4aecea8ba66267
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-08-19 14:38:03 +0200

    add new collection of functions

---
+++ R/NakkeHighcharts.R
@@ -0,0 +1,49 @@
+#' Collection of functions to provide Highchart objects
+#' 
+#' Yes, still a collection of functions...
+#' 
+#' @param hoved Main data vector
+#' @return h1 Highchart object
+#' @export
+
+AndelerHighchart <- function(hoved, AntHoved, NHoved, rest, AntRest, NRest,
+                             retn, subtxt, grtxt, shtxt, fargeHoved, fargeRest,
+                             medSml, Tittel) {
+ 
+  if (retn == 'H') {
+    chartType <- "bar"
+  } else {
+    chartType <- "column"
+  }
+  # to use extra data in tooltips, make a data series from data frame
+  df <- data.frame(y = hoved, N = as.vector(AntHoved),
+                   stringsAsFactors = FALSE)
+  ds <- rlist::list.parse(df)
+  names(ds) <- NULL
+  
+  h1 <- highcharter::highchart() %>%
+    hc_title(text = Tittel) %>%
+    hc_xAxis(title = list(text=subtxt), categories=grtxt) %>%
+    hc_yAxis(title = list(text='Andel pasienter (%)')) %>%
+    hc_add_series(name = paste0(shtxt, " (N=", NHoved, ")"),
+                  data = ds,
+                  type = chartType, color = fargeHoved) %>%
+    hc_tooltip(formatter = JS("function() { return '<b>' + this.series.name +
+                              '</b><br>' +
+                              'Andel = ' + this.y + '<br>' +
+                              'N (' + this.x + ') = ' + this.point.N; }")) %>%
+    hc_exporting(enabled = TRUE)
+  if (medSml == 1) {
+    df <- data.frame(y = rest, N = as.vector(AntRest),
+                     stringsAsFactors = FALSE)
+    ds <- rlist::list.parse(df)
+    names(ds) <- NULL
+    h1 <- hc_add_series(h1, name = paste0("Landet forøvrig (N=", NRest, ")"),
+                        data = ds,
+                        type = "scatter", color = fargeRest,
+                        marker = list(symbol = "diamond", radius = 5))
+  }
+  
+  return(h1)
+}
+
