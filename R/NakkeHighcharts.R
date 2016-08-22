fbfc74478e7978a35d27241fb614f4cd764caa0f
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-08-22 13:04:49 +0200

    extend with hc for andelerGrVar

--- R/NakkeHighcharts.R
+++ R/NakkeHighcharts.R
@@ -2,7 +2,7 @@
 #' 
 #' Yes, still a collection of functions...
 #' 
-#' @param hoved Main data vector
+#' @inheritParams FigAndeler
 #' @return h1 Highchart object
 #' @export
 
@@ -47,3 +47,48 @@ AndelerHighchart <- function(hoved, AntHoved, NHoved, rest, AntRest, NRest,
   return(h1)
 }
 
+
+#' Collection of functions to provide Highchart objects
+#' 
+#' Yes, still a collection of functions...
+#' 
+#' @inheritParams FigAndelerGrVar
+#' @return h1 Highchart object
+#' @export
+
+AndelerGrVarHighchart <- function(AndelerGrSort, Ngrtxt, AndelHele, N, Tittel,
+                                  utvalgTxt, GrNavnSort, farger) {
+  
+  # to use extra data in tooltips, make a data series from data frame
+  df <- data.frame(y = as.vector(AndelerGrSort), N = Ngrtxt,
+                   stringsAsFactors = FALSE)
+  ds <- rlist::list.parse(df)
+  names(ds) <- NULL
+  
+  h1 <- highcharter::highchart() %>%
+    hc_title(text = Tittel) %>%
+    hc_subtitle(text = utvalgTxt) %>%
+    hc_xAxis(categories=GrNavnSort) %>%
+    hc_yAxis(title = list(text='Andel (%)')) %>%
+    hc_add_series(name = "Andeler",
+                  data = ds,
+                  type = "bar", color = farger[3]) %>%
+    hc_tooltip(formatter = JS("function() { return '<b>' + this.series.name +
+                              '</b><br>' +
+                              'Andel = ' + this.y + '<br>' +
+                              this.point.N; }")) %>%
+    hc_exporting(enabled = TRUE)
+  
+  # add global ratio
+  h1 <- hc_add_series(h1, name = paste0("Hele landet (", sprintf('%.1f',
+                                                                 AndelHele),
+                                        " %), N=", N),
+                      data = rep(AndelHele, length(GrNavnSort)),
+                      type = "line",
+                      color = farger[2],
+                      marker = list(enabled=FALSE),
+                      enableMouseTracking = FALSE
+  )
+
+  return(h1)
+}
