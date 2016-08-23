971ef16dbbd1d511d7b2973901fdb4f6663a5c38
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-08-23 09:36:58 +0200

    add function for hc object andelTid

--- R/NakkeHighcharts.R
+++ R/NakkeHighcharts.R
@@ -92,3 +92,51 @@ AndelerGrVarHighchart <- function(AndelerGrSort, Ngrtxt, AndelHele, N, Tittel,
 
   return(h1)
 }
+
+#' Provider of Highchart object
+#' 
+#' Provide Highchart object for AndelTid
+#' 
+#' @inheritParams FigAndelTid
+#' @return h1 Highchart object
+#' @export
+
+AndelTidHighchart <- function(Aartxt, AndelHoved, NAarHendHoved, NHovedRes,
+                              AndelRest,
+                              NAarHendRest, NSmlRes, Tittel, utvalgTxt, shtxt,
+                              smltxt, fargeHoved, fargeRest) {
+  
+  # to use extra data in tooltips, make a data series from data frame
+  df <- data.frame(y = as.vector(AndelHoved),
+                   N = as.vector(NAarHendHoved),
+                   stringsAsFactors = FALSE)
+  ds <- rlist::list.parse(df)
+  names(ds) <- NULL
+  
+  h1 <- highcharter::highchart() %>%
+    hc_title(text = Tittel) %>%
+    hc_subtitle(text = utvalgTxt) %>%
+    hc_xAxis(title = list(text="Innleggelsesår"), categories=as.character(Aartxt)) %>%
+    hc_yAxis(title = list(text='Andel (%)')) %>%
+    hc_add_series(name = paste0(shtxt, " N=", NHovedRes),
+                  data = ds,
+                  type = "line", color = fargeHoved) %>%
+    hc_tooltip(formatter = JS("function() { return '<b>' + this.series.name +
+                              '</b><br>' +
+                              'Andel = ' + this.y + '<br>' +
+                              'N = ' + this.point.N; }")) %>%
+    hc_exporting(enabled = TRUE)
+  
+  # add global ratio
+  df <- data.frame(y = as.vector(AndelRest),
+                   N = as.vector(NAarHendRest),
+                   stringsAsFactors = FALSE)
+  ds <- rlist::list.parse(df)
+  names(ds) <- NULL
+  h1 <- hc_add_series(h1, name = paste0(smltxt, " (N=", NSmlRes, ")"),
+                      data = ds,
+                      type = "line",
+                      color = fargeRest)
+  
+  return(h1)
+}
